05c5b5c05593f31847705e5313f8ab7f567c1dc5
zeeodd <evergreenskier@gmail.com>
2020-02-25 09:41:20 -0500

    OrthogonalParse Function
    1. Implemented basic OrthoParsing for dungeon text files 2. Will need to modify the function to better suit the assignment params

---
+++ DungeonParsing/Assets/Scripts/DungeonParser.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonParser : MonoBehaviour
+{
+    // Text Data Variables
+    public TextAsset data;
+    string[] linesFromfile;
+
+    // Parsing Variables
+    private int dungeonWidth;
+    private int dungeonHeight;
+    List<int> visitedRowSpaces = new List<int>();
+    List<int> visitedColSpaces = new List<int>();
+
+    private void Awake()
+    {
+        linesFromfile = data.text.Split("\n"[0]);
+        dungeonWidth = linesFromfile[0].Length;
+        dungeonHeight = linesFromfile.Length;
+
+        for (int i = 0; i < dungeonWidth; i++)
+        {
+            visitedColSpaces.Add(i);
+        }
+
+        for (int i = 0; i < dungeonHeight; i++)
+        {
+            visitedRowSpaces.Add(i);
+        }
+    }
+    void Start()
+    {
+        Debug.Log(OrthogonalParse());
+    }
+
+    private bool OrthogonalParse()
+    {
+        bool verdict = false;
+
+        // x, i = current row
+        // y, j = current col
+        for (int i = 0; i < dungeonHeight; i++)
+        {
+            for (int j = 0; j < dungeonWidth; j++)
+            {
+                if (linesFromfile[i][j] == ' ')
+                {
+                    visitedColSpaces.Remove(j);
+                    visitedRowSpaces.Remove(i);
+                }
+            }
+        }
+
+        if (visitedColSpaces.Count == 0 && visitedRowSpaces.Count == 0)
+        {
+            verdict = true;
+        }
+
+        return verdict;
+    }
+
+}
