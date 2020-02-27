b9857910685ceda89fe2e5d04029b0318e5fa233
zeeodd <evergreenskier@gmail.com>
2020-02-27 09:07:19 -0500

    Parsing Attempt 2
    Tried to do a piecewise parsing, but that didnt work at all lol will need to rethink my process

--- DungeonParsing/Assets/Scripts/DungeonParser.cs
+++ DungeonParsing/Assets/Scripts/DungeonParser.cs
@@ -13,6 +13,9 @@ public class DungeonParser : MonoBehaviour
     private int dungeonHeight;
     List<int> visitedRowSpaces = new List<int>();
     List<int> visitedColSpaces = new List<int>();
+    List<Vector2> visited = new List<Vector2>();
+
+    Dictionary<Vector2, List<Vector2>> spaces = new Dictionary<Vector2, List<Vector2>>();
 
     private void Awake()
     {
@@ -32,11 +35,12 @@ public class DungeonParser : MonoBehaviour
     }
     void Start()
     {
-        Debug.Log(OrthogonalParse());
+        Debug.Log(Parse());
     }
 
     private bool OrthogonalParse()
     {
+
         bool verdict = false;
 
         // x, i = current row
@@ -45,6 +49,7 @@ public class DungeonParser : MonoBehaviour
         {
             for (int j = 0; j < dungeonWidth; j++)
             {
+                print(dungeonWidth);
                 if (linesFromfile[i][j] == ' ')
                 {
                     visitedColSpaces.Remove(j);
@@ -56,6 +61,88 @@ public class DungeonParser : MonoBehaviour
         if (visitedColSpaces.Count == 0 && visitedRowSpaces.Count == 0)
         {
             verdict = true;
+        }
+
+        return verdict;
+    }
+
+    private bool Parse()
+    {
+        bool verdict = true;
+
+        // x, i = current row
+        // y, j = current col
+        for (int i = 0; i < dungeonHeight; i++)
+        {
+            for (int j = 0; j < dungeonWidth; j++)
+            {
+                if (linesFromfile[i][j] == ' ')
+                {
+                    bool top = false;
+                    bool left = false;
+                    bool right = false;
+                    bool bot = false;
+                    int numTrue = 0;
+
+                    // Check if not null
+                    if(i-1 < 0)
+                    {
+                        top = true;
+                        numTrue++;
+                    }
+                    if (i + 1 > dungeonWidth)
+                    {
+                        bot = true;
+                    }
+                    if (j - 1 < 0)
+                    {
+                        left = true;
+                    }
+                    if (j + 1 > dungeonHeight)
+                    {
+                        right = true;
+                    }
+
+                    if (left == false && right == false && top == false && bot == false)
+                    {
+                        if (linesFromfile[i - 1][j] != ' ' && linesFromfile[i][j - 1] != ' ' && linesFromfile[i + 1][j] != ' ' && linesFromfile[i][j + 1] != ' ')
+                        {
+                            verdict = false;
+                        }
+                    } 
+                    else
+                    {
+                        if (left == false && right == true)
+                        {
+                            if (linesFromfile[i][j - 1] == ' ')
+                            {
+                                verdict = false;
+                            }
+                        }
+                        if (right == false && left == true)
+                        {
+                            if (linesFromfile[i][j + 1] == ' ')
+                            {
+                                verdict = false;
+                            }
+                        }
+                        if (top == false && bot == true)
+                        {
+                            if (linesFromfile[i - 1][j] == ' ')
+                            {
+                                verdict = false;
+                            }
+                        }
+                        if (bot == false && top == true)
+                        {
+                            if (linesFromfile[i + 1][j] == ' ')
+                            {
+                                verdict = false;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         return verdict;
